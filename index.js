e9bf7839031cf862b7221d5e140e25e5442ad2c3
jingakyegihaji <99796173+jingakyegihaji@users.noreply.github.com>
2025-12-25 23:24:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// public 폴더 정적 파일 제공
+// public 폴더의 index.html을 자동으로 제공
 app.use(express.static(path.join(__dirname, "public")));
 
-// 헬스 체크 유지
+// 헬스 체크
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
